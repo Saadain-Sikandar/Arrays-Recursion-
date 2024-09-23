5b5f59d2d5ea255144c281ac3a9e0436834ac18b
Saadain-Sikandar <saadain4008@gmail.com>
2024-09-24 00:52:11 +0500

    Recursion in Arrays

---
+++ src/Array.java
@@ -0,0 +1,122 @@
+public class Array {
+
+
+    public static int  maxNum (int[] arr,int index){
+
+        if (index == arr.length-1){             // IF the loop run till the end it will return last index
+            return  arr[index];
+        }
+
+        int max = maxNum(arr, index + 1);       // will increment the index after each recursion.
+
+        if(arr[index] > max){
+            return  arr[index];             // if the array index value is > than max value, it will return the index value.
+        }
+        else {
+            return max;
+        }
+    }
+
+
+    public static int MinNUm (int[] arr , int index){
+
+        if(index == arr.length-1){				    // IF the loop run till the end it will return last index
+
+            return arr[index];
+        }
+
+        int min = MinNUm(arr, index+1);		 // will increment the index after each recursion.
+
+        if(arr[index] < min){
+
+            return arr[index];			  // if the index value is < than max value, it will return the index value.
+
+        }
+
+        else {
+            return min;
+        }
+
+
+    }
+
+
+    public static int SumNUM(int[] arr, int index) {
+
+        if (index == arr.length - 1) {
+                                        // IF the loop run till the end it will return last index
+            return arr[index];
+        }
+
+
+        int sum = SumNUM(arr, index + 1);			 // will increment the index after each recursion.
+
+        sum = sum + arr[index];				// sum index values after recursion and will be save in sum.
+
+        return sum;
+
+    }
+
+
+    public static int SumArr(int[] arr1 ,int[]arr2, int index) {
+
+        if(index == arr1.length || index == arr2.length){				// if the index number reach at the end it will return 0
+
+            return 0;
+
+        }
+
+        int arrsum = SumArr(arr1,arr2,index+1);		// the recursive statement will increment the index after each recursion
+
+
+        arrsum = arr1[index] + arr2[index];		// the Addition performed will be save in arrsum variable
+
+
+        System.out.println("The Sum of Array1 and Array2 at index " + index +" is:" + arrsum);
+
+        return arrsum;
+
+    }
+
+
+
+    public static int ArrSubtraction(int[] arr1 ,int[]arr2, int index) {
+
+        if(index==arr1.length||index==arr2.length){
+            // if the index number reach at the end it will return 0
+            return 0 ;
+        }
+
+        int sub = ArrSubtraction(arr1,arr2,index+1); 		  // the recursive statement will increment the index after each recursion
+
+
+        sub = arr1[index] - arr2[index];           	 // the Subtraction performed will be save in Sub variable
+
+
+        System.out.println("The Subtraction of Array 1 and Array 2 at index "+ index +" is:"+sub);
+
+        return sub;
+
+    }
+
+
+    public static int ArrMultiplication(int[] arr1 ,int[]arr2, int index) {
+
+        if(index==arr1.length||index==arr2.length){
+                                                      // if the index number reach at the end it will return 0
+            return 0 ;
+        }
+
+        int mul = ArrMultiplication(arr1,arr2,index+1);  	 // the recursive statement will increment the index after each recursion
+
+        mul = arr1[index] * arr2[index];          		  // the Multiplication performed will be saved in mul variable
+
+        System.out.println("The Subtraction of Array 1 and Array 2 at index "+ index +" is:"+mul);
+
+        return mul;
+
+    }
+
+    
+}
+
---
+++ src/Main.java
@@ -0,0 +1,61 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //MAXIMUM NUMBER
+
+        int[] arr1 = new int[]{10, 20, 90, 60, 80, 70};
+
+        int maxNumber = Array.maxNum(arr1, 0);
+
+        System.out.println("Max number is " + maxNumber);
+
+
+        //MINIMUM NUMBER
+
+//        int [] arr1 = new int []  {50,40,60,20,10,70,80};
+//
+//        int min = Array.MinNUm(arr1,0);
+//
+//        System.out.println("The Minimum Number is:"+min);
+
+
+        // SUM OF ARRAY ELEMENTS
+
+//        int [] arr1 = new int []  {50,40,60,20,10,70,80};
+//
+//        int sum = Array.SumNUM(arr1,0);
+//
+//        System.out.println("The Sum of Array is:"+sum);
+
+
+        // SUM OF  ARRAY 1 AND ARRAY 2
+
+//        int [] arr1 = new int [] {10,20,40,50,60,70,80};
+//        int [] arr2 = new int [] {10,20,40,50,60,70,80};
+//
+//        Array.SumArr(arr1,arr2,0);
+
+
+        // SUBTRACTION OF  ARRAY 1 AND ARRAY 2
+
+//        int [] arr1 = new int [] {20,40,60,80,100,120,140};
+//        int [] arr2 = new int [] {10,20,40,50,60,70,80};
+//
+//        Array.ArrSubtraction(arr1,arr2,0);
+//
+//
+
+        // MULTIPLICATION OF  ARRAY 1 AND ARRAY 2
+
+//        int [] arr1 = new int [] {4,6,5,6,7,8,9};
+//        int [] arr2 = new int [] {5,7,9,6,4,3,2};
+//
+//        Array.ArrMultiplication(arr1,arr2,0);
+
+    }
+
+}
